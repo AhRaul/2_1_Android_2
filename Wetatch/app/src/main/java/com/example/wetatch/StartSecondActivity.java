e54d3a8c831ef18f13b12e1660586f51bbfc7874
gg <gartanolio@yandex.ru>
2019-02-26 01:05:24 +0300

    2_1_ ДЗ3 Выполнено доп. задание со звездочкой:
    3. * Добавьте на первый экран, помимо поля для города, дополнительные параметры
    (влажность, скорость ветра, давление и тд) в виде чекбоксов или свитчей.
    Передавайте их значения на второй экран и выводите дополнительную информацию в зависимости от значений.

--- Wetatch/app/src/main/java/com/example/wetatch/StartSecondActivity.java
+++ Wetatch/app/src/main/java/com/example/wetatch/StartSecondActivity.java
@@ -4,9 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Switch;
 
 public class StartSecondActivity implements View.OnClickListener{
-    public final static String TEXT = "Введите город";
+    //кючевые слова интентов
+    public final static String TEXT = "TEXT";
+    public final static String SWITCH_ATMOS = "ATMOS";
+    public final static String SWITCH_WIND = "WIND";
 
     private Activity sourceActivity;
 
@@ -17,8 +21,12 @@ public class StartSecondActivity implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         EditText txt = (EditText) sourceActivity.findViewById(R.id.editTextCityName);
+        Switch switchAtmos = (Switch)sourceActivity.findViewById(R.id.switchAtmos);     //состояние свича атмосфера
+        Switch switchWind = (Switch)sourceActivity.findViewById(R.id.switchWind);       //состояние свича ветер
         Intent intent = new Intent(sourceActivity, OutInfoActivity.class);
         intent.putExtra(TEXT, txt.getText().toString());
+        intent.putExtra(SWITCH_ATMOS, switchAtmos.isChecked());                         //занесение состояния свитча в интент
+        intent.putExtra(SWITCH_WIND, switchWind.isChecked());                           //занесение состояния свитча в интент
         sourceActivity.startActivity(intent);
     }
 }
