f09ebacc01b75325d20605cb63f4bede118306e9
gg <gartanolio@yandex.ru>
2019-03-04 23:02:35 +0300

    2_1_HW5 Выполнено задание 2: 2. В погодном приложении создайте заготовку на основе приложения CityInfo. Подготовьте фрагмент с выводом температурных характеристик вместо герба: чтобы выбрав город, можно было посмотреть температуру в нем.

--- Wetatch/app/src/main/java/com/example/wetatch/MainActivity.java
+++ Wetatch/app/src/main/java/com/example/wetatch/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.wetatch;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button = (Button)findViewById(R.id.buttonShowWeather);
-        button.setOnClickListener(new StartSecondActivity(this));
+        new ActualChoiceState("", false, false);  //первая активация статиков состояний
     }
 }
--- Wetatch/app/src/main/java/com/example/wetatch/OutInfoActivity.java
+++ Wetatch/app/src/main/java/com/example/wetatch/OutInfoActivity.java
@@ -2,61 +2,29 @@ package com.example.wetatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import static android.view.View.INVISIBLE;
-import static com.example.wetatch.StartSecondActivity.SWITCH_ATMOS;
-import static com.example.wetatch.StartSecondActivity.SWITCH_WIND;
-import static com.example.wetatch.StartSecondActivity.TEXT;
+import static android.content.res.Configuration.ORIENTATION_LANDSCAPE;
 
 public class OutInfoActivity extends AppCompatActivity {
-    String text;
-    boolean atmos;
-    boolean wind;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_out_info);
 
-        try {
-            text = getIntent().getExtras().getString(TEXT);
-            atmos = getIntent().getExtras().getBoolean(SWITCH_ATMOS);   //получаем состояние свитча из интерна
-            wind = getIntent().getExtras().getBoolean(SWITCH_WIND);     //получаем состояние свитча из интерна
-        } catch (NullPointerException e) {
-            System.out.println("getIntent == null " + e);
+        if (getResources().getConfiguration().orientation == ORIENTATION_LANDSCAPE) {
+            // Если устройство перевернули в альбомную ориентацию, то Activity надо закрыть
+            finish();
+            return;
         }
 
-        //вывод названия города
-        TextView textViewCityName = (TextView) findViewById(R.id.textViewCityName);
-        textViewCityName.setText(text);
+        if (savedInstanceState == null) {
+            // Если Activity запускается первый раз
+            // то добавим фрагмент в Activity
+            OutInfoFragment details = new OutInfoFragment();
 
-        //вывод данных о погоде
-        TextView textViewTemperatureValue = (TextView) findViewById(R.id.textViewTemperatureValue);
-        textViewTemperatureValue.setText("Данные о погоде города: ");
-        textViewTemperatureValue.append(text);
-
-        //вывод давления (если включен свитч)
-        TextView textViewOutAtmos = (TextView) findViewById(R.id.textViewOutAtmos);
-        if(atmos) {
-            textViewOutAtmos.setVisibility(View.VISIBLE);
-            textViewOutAtmos.setText("Данные о давлении города: ");
-            textViewOutAtmos.append(text);
-        } else {
-            textViewOutAtmos.setVisibility(INVISIBLE);
-        }
-
-        //вывод направления ветра (если включен свитч)
-        TextView textViewOutWind = (TextView) findViewById(R.id.textViewOutWind);
-        if(wind) {
-            textViewOutWind.setVisibility(View.VISIBLE);
-            textViewOutWind.setText("Данные о скорости ветра города: ");
-            textViewOutWind.append(text);
-        } else  {
-            textViewOutWind.setVisibility(View.INVISIBLE);
+            //добавим фрагмент в активити                       //?? как работает ??
+            getSupportFragmentManager().beginTransaction()
+                    .add(android.R.id.content, details).commit();
         }
     }
 }
